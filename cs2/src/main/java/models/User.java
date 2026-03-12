a71d3b8823f721a5b48bd79bbf6eecdf31d5fd82
sebastianvasquez62 <sebastian.vasquez62@correo.tdea.edu.co>
2026-03-11 21:14:56 -0500

    comment

---
+++ cs2/src/main/java/models/User.java
@@ -0,0 +1,14 @@
+package models;
+
+public class User {
+    private int idUser;
+    private String idRelation;
+    private String fullName;
+    private String idIdentification;
+    private String email;
+    private String phone;
+    private String date;
+    private String adress;
+    private String rolSystem;
+    private String statusUser;
+}
